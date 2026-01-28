f437a3d6f7b49bae37bd45fc91257f10f646eb05
Gregory Chebatyuk <gchebatyuk@gmail.com>
2026-01-28 10:08:29 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+export const App = () => {
+	return (
+		<>
+			<div className={styles.App}>Hello Blog</div>
+			<div>
+				<p>`So... Let`s get started`</p>
+			</div>
+		</>
+	);
+};
